ef44475fc14b0ddaba3dbba760ce3c14839e6915
Tomas Pineda <tomaspinedanaranjo@gmail.com>
2023-09-24 18:33:19 -0500

    base de  registro

--- MagnetoFavor/urls.py
+++ MagnetoFavor/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
 ]
