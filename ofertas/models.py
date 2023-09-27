a225cc055f7f203226345d33e01f73cf4cc775cc
Tomas Pineda <tomaspinedanaranjo@gmail.com>
2023-09-26 20:42:07 -0500

    Adelanto de publicar oferta

--- ofertas/models.py
+++ ofertas/models.py
@@ -7,7 +7,8 @@ class Ofertas(models.Model):
     description = models.TextField(blank = True)
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-   
+    class Meta:
+        verbose_name_plural = "Ofertas"
     def __str__(self):
         return self.title + '- by ' + self.user.username
    
