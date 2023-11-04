ddc98c522835fbaf9c23590990b1e6384a87939e
Tomas Pineda <tomaspinedanaranjo@gmail.com>
2023-11-04 18:13:01 -0500

    correcciones

--- ofertas/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Ofertas
-# Register your models here.
-
-admin.site.register(Ofertas)
--- ofertas/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Ofertas
-
-class Formulario_Oferta(ModelForm):
-    class Meta:
-        model = Ofertas
-        fields = ['title', 'description']
